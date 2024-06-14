66ec8a79134deb32c007e7ea8eac80413136899f
Peter Thomas Barrow <peter.barrow.93@gmail.com>
2024-06-14 13:03:47 +0100

    chore: updated interface (.pyi) files

---
+++ tangy/tangy/__init__.pyi
@@ -0,0 +1,2 @@
+from ._tangy import PTUFile as PTUFile, Records as Records, TangyBuffer as TangyBuffer, TangyBufferType as TangyBufferType, buffer_list_append as buffer_list_append, buffer_list_delete_all as buffer_list_delete_all, buffer_list_show as buffer_list_show, buffer_list_update as buffer_list_update, tangy_config_location as tangy_config_location
+from ._uqd import UQDLogic16 as UQDLogic16
